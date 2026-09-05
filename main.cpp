abaf3365d83ccdc7b6c5b9ba042fbfa5a0539f9a
Mohamed Zaid <mohadmaj47@outlook.com>
2026-09-05 20:45:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@ int Enemy_point = 0;
 
 char Enemy_bot(u32);
 char Player_one(void);
+char Player_two(void);
+
 void QUIT(void);
 
 
@@ -72,7 +74,7 @@ int main (void){
 
         }
     }while(user_choice != 'Q' || user_choice != 'q');
-    
+
  return 0;
 }
 
@@ -109,6 +111,42 @@ char Enemy_bot(u32 seed){
 
 
 char Player_one(void){
+    int  choice = 0;
+    
+    while(1){
+
+         std::cout << "Select your move\n Enter: \n 1 for Rock \n 2 for paper \n 3 for Scissors \n:";
+         std::cin >> choice;
+       
+        if(choice == 1){
+            return 'R';
+            break;
+        }
+    
+        else if(choice ==2){
+            return 'P';
+            break;
+        }
+        
+        else if(choice == 3){
+            return 'S';
+            break;
+        }
+
+        else{
+            std::cout << "invalid input, Please try again \n";
+        }
+    }
+}
+
+void QUIT(void){
+std::cout << "Quit \n";
+
+}
+
+
+
+char Player_two(void){
     int  choice = 0;
     
     while(1){
